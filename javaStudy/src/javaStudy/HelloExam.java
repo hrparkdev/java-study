30969089e04e005ad8d3bc49600efa253b442022
hrparkdev <hrparkdev@gmail.com>
2022-06-26 15:07:00 +0900

    Package

---
+++ javaStudy/src/javaStudy/HelloExam.java
@@ -0,0 +1,14 @@
+package javaStudy;
+
+import com.eightcruz.javaStudy.Hello;
+
+public class HelloExam {
+
+	public static void main(String[] args) {
+
+		Hello hello = new Hello();
+		
+		
+	}
+
+}
